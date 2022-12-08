3f6d7c1da9dae7de8bec9f253fadc59a12509ef6
Armen <st.armen91@gmail.com>
2022-12-08 18:09:50 +0400

    Change to manifest version 3

--- background.js
+++ background.js
@@ -1,23 +1,6 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-    navigateOptionPage();
+chrome.action.onClicked.addListener(function(tab) {
+	chrome.runtime.openOptionsPage();
 })
-
-
-function navigateOptionPage() {
-    var optionsUrl = chrome.extension.getURL('options/index.html');
-    chrome.tabs.query({}, function(extensionTabs) {
-        var found = false;
-        for (var i=0; i < extensionTabs.length; i++) {
-            if ( extensionTabs[i].url.includes(optionsUrl)) {
-                found = true;
-                chrome.tabs.update(extensionTabs[i].id, {"active": true, url: extensionTabs[i].url});
-            }
-        }
-        if (found === false) {
-            chrome.tabs.create({url: "options/index.html"});
-        }
-    });
-}
 
 // listen message from content script
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {    
